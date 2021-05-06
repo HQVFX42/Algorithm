6549dde7c306f4415b1c644cc5d3015efc95b07f
HLGFX <ford6313@gmail.com>
2021-05-07 01:35:22 +0900

    Practice Stack02

--- Stack/PracticeStack/1406_에디터/1406.cpp
+++ Stack/PracticeStack/1406_에디터/1406.cpp
@@ -1,9 +1,54 @@
-#include <iostream>
+#include <cstdio>
+#include <cstring>
 #include <stack>
-#include <string>
+#pragma warning(disable: 4996)
+
 using namespace std;
+char a[600000];
 
-int main()
-{
-
+int main() {
+	scanf("%s", a);
+	stack<char> left, right;
+	int n = strlen(a);
+	for (int i = 0; i < n; i++) {
+		left.push(a[i]);
+	}
+	int m;
+	scanf("%d", &m);
+	while (m--) {
+		char what;
+		scanf(" %c", &what);
+		if (what == 'L') {
+			if (!left.empty()) {
+				right.push(left.top());
+				left.pop();
+			}
+		}
+		else if (what == 'D') {
+			if (!right.empty()) {
+				left.push(right.top());
+				right.pop();
+			}
+		}
+		else if (what == 'B') {
+			if (!left.empty()) {
+				left.pop();
+			}
+		}
+		else if (what == 'P') {
+			char c;
+			scanf(" %c", &c);
+			left.push(c);
+		}
+	}
+	while (!left.empty()) {
+		right.push(left.top());
+		left.pop();
+	}
+	while (!right.empty()) {
+		printf("%c", right.top());
+		right.pop();
+	}
+	printf("\n");
+	return 0;
 }
