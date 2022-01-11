741cc2e398b7ef0b5ec45e39748d893945304aba
LLGFX <ford6313@gmail.com>
2022-01-11 21:53:11 +0900

    update BFS_test
    7569_토마토 (3차원 BFS)

--- PracticeCodingTest/Practice0x09_BFS/test.cpp
+++ PracticeCodingTest/Practice0x09_BFS/test.cpp
@@ -1,89 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char board[102][102];
-bool hasVis[102][102];
-int dx[4] = { 1,-1,0,0 };
-int dy[4] = { 0,0,1,-1 };
-queue<pair<char, char >> q;
-int n, ansN, ansY;
+int board[102][102][102], dist[102][102][102];
+int dx[6] = { 1,-1,0,0,0,0 }, dy[6] = { 0,0,1,-1,0,0 }, dz[6] = { 0,0,0,0,1,-1 };
+int m, n, h, ans;
+queue<tuple<int, int, int>> q;
 
-void bfs(int i, int j)
-{
-	q.push({ i,j });
-	hasVis[i][j] = true;
-	while (!q.empty())
-	{
-		auto cur = q.front(); q.pop();
-		for (int dir = 0; dir < 4; dir++)
-		{
-			int nx = cur.first + dx[dir];
-			int ny = cur.second + dy[dir];
-			if (nx < 0 || nx >= n || ny < 0 || ny >= n)
-			{
-				continue;
-			}
-			if (hasVis[nx][ny] == true || board[i][j] != board[nx][ny])
-			{
-				continue;
-			}
-			hasVis[nx][ny] = true;
-			q.push({ nx,ny });
-		}
-	}
-}
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	cin >> n;
-	for (int i = 0; i < n; i++)
+	cin >> m >> n >> h;
+	for (int i = 0; i < h; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			cin >> board[i][j];
-		}
-	}
-		
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if (hasVis[i][j] == false)
+			for (int k = 0; k < m; k++)
 			{
-				ansN++;				
-				bfs(i, j);
+				cin >> board[j][k][i];
+				if (board[j][k][i] == 1)
+				{
+					q.push({ j,k,i });
+				}
+				if (board[j][k][i] == 0)
+				{
+					dist[j][k][i] = -1;
+				}
 			}
 		}
 	}
 
-	for (int i = 0; i < n; i++)
+	while (!q.empty())
 	{
-		fill(hasVis[i], hasVis[i] + n, false);
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
+		auto cur = q.front(); q.pop();
+		for (int dir = 0; dir < 6; dir++)
 		{
-			if (board[i][j] == 'R')
+			int nx = get<0>(cur) + dx[dir], ny = get<1>(cur) + dy[dir], nz = get<2>(cur) + dz[dir];
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m || nz < 0 || nz >= h)
 			{
-				board[i][j] = 'G';
+				continue;
 			}
+			if (dist[nx][ny][nz] >= 0)
+			{
+				continue;
+			}
+			dist[nx][ny][nz] = dist[get<0>(cur)][get<1>(cur)][get<2>(cur)] + 1;
+			q.push({ nx,ny,nz });
 		}
 	}
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < h; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			if (hasVis[i][j] == false)
+			for (int k = 0; k < m; k++)
 			{
-				ansY++;
-				bfs(i, j);
+				if (dist[j][k][i] == -1)
+				{
+					cout << -1;
+					return 0;
+				}
+				ans = max(ans, dist[j][k][i]);
 			}
 		}
 	}
-	cout << ansN << ' ' << ansY;
+	cout << ans;
+
+	/*for (int i = 0; i < h; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			for (int k = 0; k < m; k++)
+			{
+				cout << board[j][k][i];
+			}
+		}
+	}*/
 }
