eb474ddd84bdd051cad48d26d4f3953e3c09da5b
LLGFX <ford6313@gmail.com>
2022-01-12 12:18:21 +0900

    update BFS_test
    나이트의 이동

--- PracticeCodingTest/Practice0x09_BFS/test.cpp
+++ PracticeCodingTest/Practice0x09_BFS/test.cpp
@@ -1,80 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int board[102][102][102], dist[102][102][102];
-int dx[6] = { 1,-1,0,0,0,0 }, dy[6] = { 0,0,1,-1,0,0 }, dz[6] = { 0,0,0,0,1,-1 };
-int m, n, h, ans;
-queue<tuple<int, int, int>> q;
+int dist[302][302];
+int dx[8] = { 1,2,2,1,-1,-2,-2,-1 }, dy[8] = { 2,1,-1,-2,-2,-1,1,2 };
+int TC, n, x, y, xx, yy;
+queue<pair<int, int>>q;
+
+void BFS()
+{
+	while (!q.empty())
+	{
+		auto cur = q.front(); q.pop();
+		for (int dir = 0; dir < 8; dir++)
+		{
+			int nx = cur.first + dx[dir], ny = cur.second + dy[dir];
+			if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+			if (dist[nx][ny] >= 0) continue;
+			dist[nx][ny] = dist[cur.first][cur.second] + 1;
+			q.push({ nx,ny });
+		}
+	}
+}
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	cin >> m >> n >> h;
-	for (int i = 0; i < h; i++)
+	cin >> TC;
+	while (TC--)
 	{
-		for (int j = 0; j < n; j++)
+		cin >> n;
+		for (int i = 0; i < n; i++)
 		{
-			for (int k = 0; k < m; k++)
-			{
-				cin >> board[j][k][i];
-				if (board[j][k][i] == 1)
-				{
-					q.push({ j,k,i });
-				}
-				if (board[j][k][i] == 0)
-				{
-					dist[j][k][i] = -1;
-				}
-			}
+			fill(dist[i], dist[i] + n, -1);
 		}
-	}
 
-	while (!q.empty())
-	{
-		auto cur = q.front(); q.pop();
-		for (int dir = 0; dir < 6; dir++)
-		{
-			int nx = get<0>(cur) + dx[dir], ny = get<1>(cur) + dy[dir], nz = get<2>(cur) + dz[dir];
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m || nz < 0 || nz >= h)
-			{
-				continue;
-			}
-			if (dist[nx][ny][nz] >= 0)
-			{
-				continue;
-			}
-			dist[nx][ny][nz] = dist[get<0>(cur)][get<1>(cur)][get<2>(cur)] + 1;
-			q.push({ nx,ny,nz });
-		}
+		cin >> x >> y;
+		dist[x][y] = 0;
+		q.push({ x,y });
+		BFS();
+		cin >> xx >> yy;		
+		cout << dist[xx][yy] << '\n';
 	}
-
-	for (int i = 0; i < h; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			for (int k = 0; k < m; k++)
-			{
-				if (dist[j][k][i] == -1)
-				{
-					cout << -1;
-					return 0;
-				}
-				ans = max(ans, dist[j][k][i]);
-			}
-		}
-	}
-	cout << ans;
-
-	/*for (int i = 0; i < h; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			for (int k = 0; k < m; k++)
-			{
-				cout << board[j][k][i];
-			}
-		}
-	}*/
 }
