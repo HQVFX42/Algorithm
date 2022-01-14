bb87db885f394d0117c23f1b4db17378bec98400
LLGFX <ford6313@gmail.com>
2022-01-15 00:08:01 +0900

    Update BFS_test
    2667_단지 번호 붙이기

--- PracticeCodingTest/Practice0x09_BFS/test.cpp
+++ PracticeCodingTest/Practice0x09_BFS/test.cpp
@@ -1,56 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int board[102][102];
-int vis[102][102];
-int dx[4] = { 1,-1,0,0 }, dy[4] = { 0,0,1, -1 };
-int m, n, k, x, y, xx, yy;
-queue<pair<int, int>> q;
+string board[25];
+int vis[25][25];
+int dx[4] = { 1,-1,0,0 }, dy[4] = { 0,0,1,-1 };
+queue<pair<int, int>>q;
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	cin >> m >> n >> k;
-	while (k--)
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
 	{
-		cin >> x >> y >> xx >> yy;
-		for (int i = y; i < yy; i++)
-		{
-			for (int j = x; j < xx; j++)
-			{
-				board[i][j] = 1;
-			}
-		}
+		cin >> board[i];
 	}
 
-	int num = 0;
+	int count = 0;
 	vector<int> ans;
-
-	for (int i = 0; i < m; i++)
+	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			if (board[i][j] == 1 || vis[i][j] == 1)
+			if (board[i][j] == '0' || vis[i][j] == 1)
 			{
 				continue;
 			}
 			vis[i][j] = 1;
 			q.push({ i,j });
 			int width = 1;
-			num++;
+			count++;
 			while (!q.empty())
 			{
 				auto cur = q.front();	q.pop();
 				for (int dir = 0; dir < 4; dir++)
 				{
 					int nx = cur.first + dx[dir], ny = cur.second + dy[dir];
-					if (nx < 0 || nx >= m || ny < 0 || ny >= n)
+					if (nx < 0 || nx >= n || ny < 0 || ny >= n)
 					{
 						continue;
 					}
-					if (vis[nx][ny] == 1 || board[nx][ny] == 1)
+					if (board[nx][ny] == '0' || vis[nx][ny] == 1)
 					{
 						continue;
 					}
@@ -62,10 +54,8 @@ int main()
 			ans.push_back(width);
 		}
 	}
-	cout << num << '\n';
+
+	cout << count << '\n';
 	sort(ans.begin(), ans.end());
-	for (int i = 0; i < ans.size(); i++)
-	{
-		cout << ans[i] << ' ';
-	}
+	for (auto i : ans)	cout << i << '\n';
 }
