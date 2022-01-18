1334700c3d89a5dff740f63c957a918db2484e5e
LLGFX <ford6313@gmail.com>
2022-01-18 12:29:41 +0900

    update 0x02_test

--- PracticeCodingTest/Practice0x02/0x02_test.cpp
+++ PracticeCodingTest/Practice0x02/0x02_test.cpp
@@ -6,13 +6,10 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int TC;
-	cin >> TC;
-	for (int i = 0; i < TC; i++)
-	{
-		int j = 0;
-		for (; j < TC - i - 1; j++)cout << ' ';
-		for (; j < TC; j++)cout << '*';
-		cout << '\n';
-	}
+	int arr[9] = {};
+	for (int i = 0; i < 9; i++) cin >> arr[i];
+	int ans = 0;
+	for (int i = 0; i < 9; i++) ans = max(ans, arr[i]);
+	cout << ans << '\n';
+	cout << find(arr, arr + 9, ans) - arr + 1;
 }
--- PracticeCodingTest/Practice0x02/2562_최댓값.cpp
+++ PracticeCodingTest/Practice0x02/2562_최댓값.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int n, maxVal, maxId;
 
+void solution()
+{
+	int arr[9] = {};
+	for (int i = 0; i < 9; i++) cin >> arr[i];
+	int ans = 0;
+	for (int i = 0; i < 9; i++) ans = max(ans, arr[i]);
+	cout << ans << '\n';
+	cout << find(arr, arr + 9, ans) - arr + 1;
+}
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -21,3 +30,4 @@ int main()
 	}
 	cout << maxVal << "\n" << maxId;
 }
+
