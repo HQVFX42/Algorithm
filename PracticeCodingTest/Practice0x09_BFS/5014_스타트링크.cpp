5ede0d14c0c5f82f2f93011eaa141b69f5705717
LLGFX <ford6313@gmail.com>
2022-01-20 17:10:28 +0900

    update BFS_5014_스타트링크
    1차원 BFS로 해결

--- PracticeCodingTest/Practice0x09_BFS/5014_스타트링크.cpp
+++ PracticeCodingTest/Practice0x09_BFS/5014_스타트링크.cpp
@@ -1,10 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int dist[1000002];
+int F, S, G, U, D;
+queue<int> q;
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	
+	cin >> F >> S >> G >> U >> D;
+	fill(dist, dist + F + 1, -1);
+	dist[S] = 0;
+	q.push(S);
+
+	while (!q.empty())
+	{
+		auto cur = q.front(); q.pop();
+		for (auto nxt : { cur + U,cur - D })
+		{
+			if (nxt <= 0 || nxt > F || dist[nxt] != -1) continue;
+			dist[nxt] = dist[cur] + 1;
+			q.push(nxt);
+		}
+	}
+	if (dist[G] == -1) cout << "use the stairs";
+	else cout << dist[G];
 }
