056d0c439275baa30bb4dd6fc11dc23688b27a68
LLGFX <ford6313@gmail.com>
2022-01-24 00:14:29 +0900

    update BFS_2468, 6593

--- PracticeCodingTest/Practice0x09_BFS/2468_안전영역.cpp
+++ PracticeCodingTest/Practice0x09_BFS/2468_안전영역.cpp
@@ -7,38 +7,68 @@ int dx[4] = { 1,-1,0,0 }, dy[4] = { 0,0,1,-1 };
 int n;
 queue<pair<int, int>> q;
 
+void INIT()
+{
+	for (int i = 0; i < n; i++)
+	{
+		fill(vis[i], vis[i] + n, 0);
+	}
+}
+
+void BFS(int i, int j, int height)
+{
+	vis[i][j] = 1;
+	q.push({ i,j });
+	while (!q.empty())
+	{
+		auto cur = q.front();	q.pop();
+		for (int dir = 0; dir < 4; dir++)
+		{
+			int nx = cur.first + dx[dir], ny = cur.second + dy[dir];
+			if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+			if (vis[nx][ny] == 0 && board[nx][ny] > height)	// 방문하지 않았으면서, 침수되지 않았을 때
+			{
+				vis[nx][ny] = 1;
+				q.push({ nx,ny });
+			}
+		}
+	}
+	
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
 	cin >> n;
+	int maxHeight = 0;
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
 			cin >> board[i][j];
+			maxHeight = max(board[i][j], maxHeight);	// 잠기는 최대 높이 저장
 		}
 	}
-
-	int count = 0;
-	for (int i = 0; i < n; i++)
+	// 0부터 최대높이까지 반복, 전체 BFS
+	int maxCount = 0;
+	for (int height = 0; height <= maxHeight; height++)
 	{
-		for (int j = 0; j < n; j++)
+		INIT();
+		int count = 0;	// 0부터 최대높이까지 각각 최대 영역 개수 저장
+		for (int i = 0; i < n; i++)
 		{
-			if (board[i][j] != 0 || vis[i][j] == 0)
+			for (int j = 0; j < n; j++)
 			{
-				while (!q.empty())
+				if (vis[i][j] == 0 && board[i][j] > height)	// 방문 여부, 침수 여부
 				{
-					auto cur = q.front();	q.pop();
-					for (int dir = 0; dir < 4; dir++)
-					{
-						int nx = cur.first + dx[dir], ny = cur.second + dy[dir];
-						if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-						if (vis[nx][ny] == 0 && board[nx][ny] != 0) continue;
-					}
+					BFS(i,j,height);
+					count++;
 				}
 			}
 		}
+		maxCount = max(count, maxCount);
 	}
+	cout << maxCount;
 }
---
+++ PracticeCodingTest/Practice0x09_BFS/6593_상범빌딩.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char board[30][30][30];
+int vis[30][30][30];
+int dx[6] = { 1,-1,0,0,0,0 }, dy[6] = { 0,0,1,-1,0,0 }, dz[6] = { 0,0,0,0,1,-1 };
+int X, Y, Z;
+
+void INIT()
+{
+	for (int i = 0; i < Z; i++)
+	{
+		for (int j = 0; j < X; j++)
+		{
+			fill(vis[i][j], vis[i][j] + Y, 0);
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	while (true)
+	{
+		cin >> Z >> X >> Y;
+		if (Z == 0 && X == 0 && Y == 0)	break;
+		bool bEnd = false;
+		queue<tuple<int, int, int>> q;
+		INIT();
+
+		for (int i = 0; i < Z; i++)
+		{
+			for (int j = 0; j < X; j++)
+			{
+				for (int k = 0; k < Y; k++)
+				{
+					cin >> board[i][j][k];
+					if (board[i][j][k] == 'S')
+					{
+						q.push({ i,j,k });
+						vis[i][j][k] = 0;
+					}
+					if (board[i][j][k] == '.')
+					{
+						vis[i][j][k] = -1;
+					}
+				}
+			}
+		}
+
+		while (!q.empty())
+		{
+			if (bEnd) break;	// 안 넣으면 틀림 왜?
+			auto cur = q.front();	q.pop();
+			int curX, curY, curZ;
+			tie(curZ, curX, curY) = cur;
+			for (int dir = 0; dir < 6; dir++)
+			{
+				/*int nx = get<1>(cur) + dx[dir];
+				int ny = get<2>(cur) + dy[dir];
+				int nz = get<0>(cur) + dz[dir];*/
+				int nx = curX + dx[dir];
+				int ny = curY + dy[dir];
+				int nz = curZ + dz[dir];
+				if (nx < 0 || nx >= X || ny < 0 || ny >= Y || nz < 0 || nz >= Z)
+				{
+					continue;
+				}
+				if (vis[nz][nx][ny] > 0 || board[nz][nx][ny] == '#')
+				{
+					continue;
+				}
+				//vis[nz][nx][ny] = vis[get<0>(cur)][get<1>(cur)][get<2>(cur)] + 1;
+				vis[nz][nx][ny] = vis[curZ][curX][curY] + 1;
+				if (board[nz][nx][ny] == 'E')
+				{
+					cout << "Escaped in " << vis[get<0>(cur)][get<1>(cur)][get<2>(cur)] + 1 << " minute(s).\n";
+					bEnd = true;
+					break;
+				}
+				q.push({ nz,nx,ny });
+			}
+		}
+		while (!q.empty())
+		{
+			q.pop();
+		}
+		if (bEnd == false)
+		{
+			cout << "Trapped!\n";
+		}
+	}
+}
