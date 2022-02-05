32b9830c1e786a6f445899bc3e3cfab438e41280
LLGFX <ford6313@gmail.com>
2022-02-05 21:47:59 +0900

    update Backtracking 15651_N과 M (4) - T

---
+++ PracticeCodingTest/Practice_0x0C_Backtracking/15652_N과M(4).cpp
@@ -0,0 +1,33 @@
+// 1s, 512MB
+// 1<= M <= N <= 8
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[10];
+
+void funcRecursive(int k, int n, int m)
+{
+	if (k == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	int begin = 1;
+	if (k != 0) begin = arr[k - 1];
+	for (int i = begin; i <= n; i++)
+	{
+		arr[k] = i;
+		funcRecursive(k + 1, n, m);
+	}
+}
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	funcRecursive(0, n, m);
+}
