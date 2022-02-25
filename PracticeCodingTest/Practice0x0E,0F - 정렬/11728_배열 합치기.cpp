a25e35bec1f53265884d9946922556ceeefccb5a
LLGFX <ford6313@gmail.com>
2022-02-26 01:31:26 +0900

    update 정렬_test

--- PracticeCodingTest/Practice0x0E,0F - 정렬/11728_배열 합치기.cpp
+++ PracticeCodingTest/Practice0x0E,0F - 정렬/11728_배열 합치기.cpp
@@ -1,3 +1,4 @@
+// merge sort
 #include <bits/stdc++.h>
 using namespace std;
 
---
+++ PracticeCodingTest/Practice0x0E,0F - 정렬/test.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n = 10;
+int arr[1000001] = { 15, 25, 22, 357, 16, 23, -53, 12, 46, 3 };
+int tmp[1000001]; // merge 함수에서 리스트 2개를 합친 결과를 임시로 저장하고 있을 변수
+
+// mid = (st+en)/2라고 할 때 arr[st:mid], arr[mid:en]은 이미 정렬이 되어있는 상태일 때 arr[st:mid]와 arr[mid:en]을 합친다.
+void merge(int st, int en) {
+	int mid = (st + en) / 2;
+	int idxL = st, idxR = mid;
+	for (int i = st; i < en; i++)
+	{
+		if (idxR == en) tmp[i] = arr[idxL++];
+		else if (idxL == mid) tmp[i] = arr[idxR++];
+		else if (arr[idxL] <= arr[idxR]) tmp[i] = arr[idxL++];
+		else tmp[i] = arr[idxR++];
+	}
+	for (int i = st; i < en; i++) arr[i] = tmp[i];
+}
+
+// arr[st:en]을 정렬하고 싶다.
+void merge_sort(int st, int en) {
+	if (en == st + 1) return; // 길이 1인 경우
+	int mid = (st + en) / 2;
+	merge_sort(st, mid); // arr[st:mid]을 정렬한다.
+	merge_sort(mid, en); // arr[mid:en]을 정렬한다.
+	merge(st, en); // arr[st:mid]와 arr[mid:en]을 합친다.
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	merge_sort(0, n);
+	for (int i = 0; i < n; i++) cout << arr[i] << ' ';  // -53 3 12 15 16 22 23 25 46 357
+}
