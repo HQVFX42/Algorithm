3df63cc64e40bb7aeaf1a2bc1aae631a00a80aa9
LLGFX <ford6313@gmail.com>
2022-03-01 21:40:39 +0900

    update 정렬 10814_나이순 정렬 - T
    c++17 tuple 문법 추가

--- PracticeCodingTest/Practice0x0E,0F - 정렬/10814_나이순정렬.cpp
+++ PracticeCodingTest/Practice0x0E,0F - 정렬/10814_나이순정렬.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+// 람다식을 이용한 풀이
 void func1()
 {
 	int n;
@@ -17,7 +17,7 @@ void func1()
 		cout << i.first << ' ' << i.second << '\n';
 	}
 }
-
+// compare 메서드를 이용한 풀이
 bool compare(const pair<int, string>& lhs, const pair<int, string>& rhs)
 {
 	return lhs.first < rhs.first;
@@ -37,12 +37,31 @@ void func2()
 		cout << i.first << ' ' << i.second << '\n';
 	}
 }
-
+// tuple을 사용한 풀이
+void func3()
+{
+	int n;
+	cin >> n;
+	vector<tuple<int, int, string>> v;
+	for (int i = 0; i < n; i++)
+	{
+		int age;
+		string name;
+		cin >> age >> name;
+		v.push_back({ age,i,name });
+	}
+	sort(v.begin(), v.end());
+	for (auto [age, b, name] : v)	// c++ 17이상 지원
+	{
+		cout << age << " " << name << '\n';
+	}
+}
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
 	//func1();
-	func2();
+	//func2();
+	func3();
 }
