1b3c2a0be0c00287a0f9f842fb879078e4ead4f4
LLGFX <ford6313@gmail.com>
2022-03-12 00:38:09 +0900

    update BFS 13549_숨바꼭질 3 - F
    0-1 bfs (double ended queue)

--- PracticeCodingTest/Practice0x09_BFS/13549_숨바꼭질 3.cpp
+++ PracticeCodingTest/Practice0x09_BFS/13549_숨바꼭질 3.cpp
@@ -29,32 +29,133 @@ void tp(int nxt)
 }
 void bfs()
 {
-	fill(dist, dist + MX, -1);
-	dist[n] = 0;	
+	dist[n] = 0;
 	q.push(n);
-	tp(n);	// 순간이동으로만 k에 도착할 수 있을 경우
+	//tp(n);	// 순간이동으로만 k에 도착할 수 있을 경우
 	while (!q.empty())
 	{
 		auto cur = q.front(); q.pop();
+		if (cur == k) break;
 		for (auto& nxt : { cur - 1, cur + 1 })
 		{
-			if (nxt < 0 or nxt >= MX or dist[nxt] != -1) continue;
+			if (nxt < 0 or nxt >= MX or dist[nxt] < dist[cur] + 1) continue;
 			dist[nxt] = dist[cur] + 1;
 			q.push(nxt);
-			tp(nxt);
+			//tp(nxt);
+		}
+		if (2*cur < MX and dist[2*cur] > dist[cur])
+		{
+			dist[2 * cur] = dist[cur];
+			q.push(2*cur);
 		}
 	}
 }
 void bfs2()
 {	// 0-1BFS
-	
+	dist[n] = 0;
+	deque<int> dq;
+	dq.push_back(n);
+	while (!dq.empty())
+	{
+		auto cur = dq.front(); dq.pop_front();
+		for (auto& nxt : { cur - 1,cur + 1 })
+		{
+			if (nxt < 0 or nxt >= MX or dist[nxt] != -1) continue;
+			dist[nxt] = dist[cur] + 1;
+			dq.push_back(nxt);
+		}
+		if (2 * cur < MX and dist[2 * cur] == -1)
+		{
+			dist[2 * cur] = dist[cur];
+			dq.push_front(2 * cur);
+		}
+	}
 }
 
 int main()
 {
 	fastIO();
 	cin >> n >> k;
-	//bfs();
-	bfs2();
+	fill(dist, dist + MX, 0x0f0f0f0f);
+	bfs();
+	//bfs2();
 	cout << dist[k];
 }
+
+//#include<iostream>
+//#include<queue>
+//
+//#define endl "\n"
+//#define MAX 100000 + 1
+//#define INF 0x0f0f0f0f
+//using namespace std;
+//
+//int N, K;
+//int Visit[MAX];
+//
+//void Input()
+//{
+//    cin >> N >> K;
+//    for (int i = 0; i < MAX; i++)
+//    {
+//        Visit[i] = INF;
+//    }
+//}
+//
+//int BFS()
+//{
+//    queue<int> Q;
+//    Q.push(N);
+//    Visit[N] = 0;
+//
+//    while (Q.empty() == 0)
+//    {
+//        int Pos = Q.front();
+//        Q.pop();
+//
+//        if (Pos == K) return Visit[K];
+//
+//        if (Pos * 2 < MAX && Visit[Pos * 2] > Visit[Pos])
+//        {
+//            cout << Visit[Pos * 2] << ' ' << Visit[Pos] << endl;
+//            Visit[Pos * 2] = Visit[Pos];
+//            Q.push(Pos * 2);
+//        }
+//        if (Pos + 1 < MAX && Visit[Pos + 1] > Visit[Pos] + 1)
+//        {
+//            cout << Visit[Pos+1] << ' ' << Visit[Pos] + 1 << endl;
+//            Visit[Pos + 1] = Visit[Pos] + 1;
+//            Q.push(Pos + 1);
+//        }
+//
+//        if (Pos - 1 >= 0 && Visit[Pos - 1] > Visit[Pos] + 1)
+//        {
+//            Visit[Pos - 1] = Visit[Pos] + 1;
+//            Q.push(Pos - 1);
+//            cout << -1 << endl;
+//        }
+//
+//    }
+//}
+//
+//void Solution()
+//{
+//    int R = BFS();
+//    cout << R << endl;
+//}
+//
+//void Solve()
+//{
+//    Input();
+//    Solution();
+//}
+//
+//int main(void)
+//{
+//    ios::sync_with_stdio(false);
+//    cin.tie(NULL);
+//
+//    Solve();
+//
+//    return 0;
+//}
