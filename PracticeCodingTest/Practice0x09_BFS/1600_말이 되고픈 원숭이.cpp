c445c466abede0ea2ee03a8783633589fb7ed262
HLGFX <ford6313@gmail.com>
2022-03-12 23:17:12 +0900

    update BFS 13913_숨바꼭질 - F

---
+++ PracticeCodingTest/Practice0x09_BFS/1600_말이 되고픈 원숭이.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> pii;
+
+const int dx[] = { 1,0,-1,0 }, dy[] = { 0,1,0,-1 };
+bool OOB(int x, int y, int n, int m) { return x < 0 or x >= n or y < 0 or y >= m; }
+void fastIO() { ios::sync_with_stdio(false); cin.tie(nullptr); }
+
+int main()
+{
+	fastIO();
+
+}
