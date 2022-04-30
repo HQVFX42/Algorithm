4ea9f67be1795ef044dd4b56a0bcff611338b246
HLGFX <ford6313@gmail.com>
2022-04-30 23:15:55 +0900

    update 투포인터 2230_수 고르기 - F

---
+++ PracticeCodingTest/Practice_0x14_투포인터/2230_수 고르기.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fastIO() { ios::sync_with_stdio(false); cin.tie(nullptr); }
+
+int a[111111];
+
+int main()
+{
+	fastIO();
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	sort(a, a + n);
+
+	int ans = 0xfffffff;
+	int lp = 0, rp = 0;
+	for (lp; lp < n; lp++)
+	{
+		while (rp < n and a[rp] - a[lp] < m)
+		{
+			rp++;
+		}
+		if (rp == n) break;
+		ans = min(ans, a[rp] - a[lp]);
+	}
+	cout << ans;
+}
