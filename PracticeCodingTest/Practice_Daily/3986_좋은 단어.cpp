22b28162b9c163595d111aef4026fb4e70bfd8eb
HLGFX <ford6313@gmail.com>
2022-05-30 23:36:41 +0900

    3986_좋은 단어
    Stack을 활용한 판별 문제

---
+++ PracticeCodingTest/Practice_Daily/3986_좋은 단어.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fastIO() { ios::sync_with_stdio(false); cin.tie(nullptr); }
+
+int main()
+{
+	fastIO();
+	int n;
+	cin >> n;
+	int ans = 0;
+	for (int i = 0; i < n; i++)
+	{
+		string tmp;
+		cin >> tmp;
+		stack<char> s;
+		int tsz = tmp.size();
+		for (int j = 0; j < tsz; j++)
+		{
+			if (s.size() and s.top() == tmp[j])
+			{
+				s.pop();
+			}
+			else
+			{
+				s.push(tmp[j]);
+			}
+		}
+		if (s.size() == 0)
+		{
+			ans++;
+		}
+	}
+	cout << ans;
+}
