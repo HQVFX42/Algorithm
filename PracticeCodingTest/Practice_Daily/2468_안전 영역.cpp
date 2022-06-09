b3193a2ed6401ff6ac36f534a346c9d365575852
LLGFX <ford6313@gmail.com>
2022-06-09 22:41:31 +0900

    2468_안전 영역 - F

---
+++ PracticeCodingTest/Practice_Daily/2468_안전 영역.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> pii;
+
+const int dx[] = { 1,0,-1,0 }, dy[] = { 0,1,0,-1 };
+bool OOB(int x, int y, int n, int m) { return x < 0 or x >= n or y < 0 or y >= m; }
+void fastIO() { ios::sync_with_stdio(false); cin.tie(nullptr); }
+
+int graph[111][111], vis[111][111];
+
+void bfs(int i, int j, int n)
+{
+
+}
+
+int main()
+{
+	fastIO();
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> graph[i][j];
+		}
+	}
+	int ans = 0;
+}
