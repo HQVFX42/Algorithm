5024e794faf02dfa534a7eb92da8ba2e1e569d8a
HLGFX <ford6313@gmail.com>
2022-06-18 23:33:53 +0900

    2468_안전 영역 - T
    탐색

--- PracticeCodingTest/Practice_Daily/2468_안전 영역.cpp
+++ PracticeCodingTest/Practice_Daily/2468_안전 영역.cpp
@@ -9,9 +9,9 @@ void fastIO() { ios::sync_with_stdio(false); cin.tie(nullptr); }
 
 int graph[111][111], vis[111][111];
 
-void bfs(int i, int j, int n)
+void init(int n)
 {
-
+	for (int i = 0; i < n; i++) { fill(vis[i], vis[i] + n, 0); }
 }
 
 int main()
@@ -19,12 +19,54 @@ int main()
 	fastIO();
 	int n;
 	cin >> n;
+	int maxHeight = 0;
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
 			cin >> graph[i][j];
+			maxHeight = max(graph[i][j], maxHeight);
 		}
 	}
+
+	queue<pii> q;
 	int ans = 0;
+	for (int height = 0; height <= maxHeight; height++)
+	{
+		init(n);
+		int cnt = 0;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (graph[i][j] > height and vis[i][j] == 0)
+				{
+					vis[i][j] = 1;
+					q.push({ i,j });
+					while (!q.empty())
+					{
+						int x, y;
+						tie(x, y) = q.front(); q.pop();
+						for (int dir = 0; dir < 4; dir++)
+						{
+							int nx = x + dx[dir], ny = y + dy[dir];
+							if (OOB(nx,ny,n,n))
+							{
+								continue;
+							}
+							if (vis[nx][ny] == 0 and graph[nx][ny] > height)
+							{
+								vis[nx][ny] = 1;
+								q.push({ nx,ny });
+							}
+						}
+					}
+					cnt++;
+				}
+			}
+		}
+		ans = max(cnt, ans);
+	}
+
+	cout << ans;
 }
