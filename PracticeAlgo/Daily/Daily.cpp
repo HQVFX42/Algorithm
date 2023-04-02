7e9c11209ab39def5580d8f1ab143ddcea836ab5
HLGFX <ford6313@gmail.com>
2023-04-02 23:57:43 +0900

    2828

--- PracticeAlgo/Daily/Daily.cpp
+++ PracticeAlgo/Daily/Daily.cpp
@@ -15,223 +15,37 @@ int graph[MAX][MAX];
 int vis[MAX][MAX];
 string a[MAX];
 
-string split(string input, string delimiter)
-{
-	vector<string> ret;
-	long long pos = 0;
-	string token = "";
-	while ((pos = input.find(delimiter)) != string::npos)
-	{
-		token = input.substr(0, pos);
-		ret.push_back(token);
-		input.erase(0, pos + delimiter.length());
-	}
-	ret.push_back(input);
-
-	string output;
-	for (auto& i : ret)
-	{
-		output += i;
-	}
-	return output;
-}
-
-struct NodeV
-{
-	int color; // 방의 색 (0: 흰색, 1: 빨간색, 2: 파란색)
-	vector<int> adj; // 인접한 방의 번호
-};
-
-vector<NodeV> nodes;
-int n, m, res;
-
-int bfs(int start)
-{
-	queue<int> q;
-	vector<bool> visited(n + 1, false);
-	int Cnt = 0;
-	q.push(start);
-	visited[start] = true;
-	nodes[start].color = 1;
-	Cnt++;
-
-	while (!q.empty())
-	{
-		int cur = q.front();
-		q.pop();
-
-		for (int next : nodes[cur].adj)
-		{
-			if (!visited[next])
-			{
-				if (nodes[cur].color == 1)
-				{
-					nodes[next].color = 2;
-				}
-				else if (nodes[cur].color == 2)
-				{
-					nodes[next].color = 1;
-					Cnt++;
-				}
-
-				q.push(next);
-				visited[next] = true;
-			}
-		}
-	}
-
-	return Cnt;
-}
-
-void dfs(int x, int y)
-{
-	vis[x][y] = 1;
-	for (int i = 0; i < 4; i++)
-	{
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		if (OOB(nx, ny, n, m))
-		{
-			continue;
-		}
-		if (graph[nx][ny] == 1 and vis[nx][ny] == 0)
-		{
-			dfs(nx, ny);
-		}
-	}
-	return;
-
-}
-
-int dfsI(int x, int y)
-{
-	int res = 1;
-
-	vis[x][y] = 1;
-	for (int i = 0; i < 4; i++)
-	{
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		if (OOB(nx, ny, m, n))
-		{
-			continue;
-		}
-		if (graph[nx][ny] == 1 or vis[nx][ny] == 1)
-		{
-			continue;
-		}
-		res += dfsI(nx, ny);
-	}
-	return res;
-}
-
-bool Check(int d, int x, int y)
-{
-	for (int i = x; i < x + d; i++)
-	{
-		for (int j = y; j < y + d; j++)
-		{
-			if (a[i][j] != a[x][y])
-			{
-				return false;
-			}
-		}
-	}
-	return true;
-}
-
-void dfs(int d, int x, int y)
-{
-	if (Check(d, x, y))
-	{
-		cout << a[x][y];
-		return;
-	}
-
-	cout << '(';
-	float divide = 0.5;
-	for (int i = 0; i < 2; i++)
-	{
-		for (int j = 0; j < 2; j++)
-		{
-			dfs(d * divide, x + i * d * divide, y + j * d * divide);
-		}
-	}
-	cout << ')';
-}
-
-map<int, vector<int>> g;
-set<int> visited;
-
-int dfs(int depth, set<int>& visited)
-{
-	int cnt = 1;
-	visited.insert(depth);
-	for (auto& i : g[depth])
-	{
-		if (!visited.count(i))
-		{
-			cnt += dfs(i, visited);
-		}
-	}
-
-	return cnt;
-}
-
 int main()
 {
-	int M;
 	int N;
-	cin >> M >> N;
-	for (int i = 0; i < M; i++)
-	{
-		int u;
-		int v;
-		cin >> u >> v;
-		g[u].push_back(v);
-		g[v].push_back(u);
-	}
+	int M;
+	int J;
+	cin >> N >> M >> J;
+	int l = 1;
 
 	int res = 0;
-	for (auto& i : g)
+	while (J--)
 	{
-		if (!visited.count(i.first))
+		int r = l + M - 1;
+		int temp;
+		cin >> temp;
+		if (temp >= l and temp <= r)
 		{
-			int count = dfs(i.first, visited);
-			if (count <= N)
-			{
-				res += count;
-			}
+			continue;
+		}
+		if (temp < l)
+		{
+			res += l - temp;
+			l = temp;
+		}
+		else
+		{
+			res += temp - r;
+			l += temp - r;
 		}
 	}
-	cout << res << '\n';
+
+	cout << res;
 
 	return 0;
 }
-
-
-/*
-10 3
-21 88
-23 75
-97 35
-2 8
-67 9
-64 75
-65 71
-70 98
-9 71
-60 35
-
-10
-abcd
-abcde
-sdkfkkoxcc
-dkfuds
-kdgugs
-dkfdu
-dsf
-dkjfgusdgg
-dkdkfdufd
-kkoxcc
-*/
--- PracticeAlgo/PracticeAlgo/Messing.cpp
+++ PracticeAlgo/PracticeAlgo/Messing.cpp
@@ -7,6 +7,169 @@ const int dx[] = { 1,0,-1,0 }, dy[] = { 0,1,0,-1 };
 bool OOB(int x, int y, int n, int m) { return x < 0 or x >= n or y < 0 or y >= m; }
 void fastIO() { ios::sync_with_stdio(false); cin.tie(nullptr); }
 
+string split(string input, string delimiter)
+{
+	vector<string> ret;
+	long long pos = 0;
+	string token = "";
+	while ((pos = input.find(delimiter)) != string::npos)
+	{
+		token = input.substr(0, pos);
+		ret.push_back(token);
+		input.erase(0, pos + delimiter.length());
+	}
+	ret.push_back(input);
+
+	string output;
+	for (auto& i : ret)
+	{
+		output += i;
+	}
+	return output;
+}
+
+struct NodeV
+{
+	int color; // 방의 색 (0: 흰색, 1: 빨간색, 2: 파란색)
+	vector<int> adj; // 인접한 방의 번호
+};
+
+vector<NodeV> nodes;
+int n, m, res;
+
+int bfs(int start)
+{
+	queue<int> q;
+	vector<bool> visited(n + 1, false);
+	int Cnt = 0;
+	q.push(start);
+	visited[start] = true;
+	nodes[start].color = 1;
+	Cnt++;
+
+	while (!q.empty())
+	{
+		int cur = q.front();
+		q.pop();
+
+		for (int next : nodes[cur].adj)
+		{
+			if (!visited[next])
+			{
+				if (nodes[cur].color == 1)
+				{
+					nodes[next].color = 2;
+				}
+				else if (nodes[cur].color == 2)
+				{
+					nodes[next].color = 1;
+					Cnt++;
+				}
+
+				q.push(next);
+				visited[next] = true;
+			}
+		}
+	}
+
+	return Cnt;
+}
+
+void dfs(int x, int y)
+{
+	vis[x][y] = 1;
+	for (int i = 0; i < 4; i++)
+	{
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		if (OOB(nx, ny, n, m))
+		{
+			continue;
+		}
+		if (graph[nx][ny] == 1 and vis[nx][ny] == 0)
+		{
+			dfs(nx, ny);
+		}
+	}
+	return;
+
+}
+
+int dfsI(int x, int y)
+{
+	int res = 1;
+
+	vis[x][y] = 1;
+	for (int i = 0; i < 4; i++)
+	{
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		if (OOB(nx, ny, m, n))
+		{
+			continue;
+		}
+		if (graph[nx][ny] == 1 or vis[nx][ny] == 1)
+		{
+			continue;
+		}
+		res += dfsI(nx, ny);
+	}
+	return res;
+}
+
+bool Check(int d, int x, int y)
+{
+	for (int i = x; i < x + d; i++)
+	{
+		for (int j = y; j < y + d; j++)
+		{
+			if (a[i][j] != a[x][y])
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+void dfs(int d, int x, int y)
+{
+	if (Check(d, x, y))
+	{
+		cout << a[x][y];
+		return;
+	}
+
+	cout << '(';
+	float divide = 0.5;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			dfs(d * divide, x + i * d * divide, y + j * d * divide);
+		}
+	}
+	cout << ')';
+}
+
+map<int, vector<int>> g;
+set<int> visited;
+
+int dfs(int depth, set<int>& visited)
+{
+	int cnt = 1;
+	visited.insert(depth);
+	for (auto& i : g[depth])
+	{
+		if (!visited.count(i))
+		{
+			cnt += dfs(i, visited);
+		}
+	}
+
+	return cnt;
+}
+
 class A
 {
 public:
@@ -70,6 +233,33 @@ int main()
 	ptrA->print();
 	ptrB->print();
 
-	delete ptrA;
+	//delete ptrA;
 	delete ptrB;
 }
+
+
+/*
+10 3
+21 88
+23 75
+97 35
+2 8
+67 9
+64 75
+65 71
+70 98
+9 71
+60 35
+
+10
+abcd
+abcde
+sdkfkkoxcc
+dkfuds
+kdgugs
+dkfdu
+dsf
+dkjfgusdgg
+dkdkfdufd
+kkoxcc
+*/
