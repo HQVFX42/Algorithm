2ff5d594605928c8a6c42c43325a4ae6a8cba604
Carroll42 <ford6313@gmail.com>
2024-12-27 19:16:02 +0900

    BOj 17298

--- Algothingy/17298.cpp
+++ Algothingy/17298.cpp
@@ -7,8 +7,30 @@ const int dx[] = { 1, 0, -1, 0 }, dy[] = { 0, 1, 0, -1 };
 bool OOB(int x, int y, int n, int m) { return x < 0 or x >= n or y < 0 or y >= m; }
 void FastIO() { ios::sync_with_stdio(false); cin.tie(nullptr); }
 
+const int MAX = 1111111;
+stack<int> Sta;
+int N, A[ MAX ], R[ MAX ];
+
 int main()
 {
 	FastIO();
+	
+	memset(R, -1, sizeof(R));
 
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> A[ i ];
+		while (Sta.size() and A[Sta.top()] < A[i])
+		{
+			R[ Sta.top() ] = A[ i ];
+			Sta.pop();
+		}
+		Sta.push(i);
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << R[ i ] << " ";
+	}
 }
