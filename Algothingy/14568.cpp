dc80270d9a4914f64080fb93bb6a7455125075ad
Carroll42 <ford6313@gmail.com>
2025-01-01 23:48:41 +0900

    BOJ 14568

---
+++ Algothingy/14568.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef long long ll;
+
+#define X first
+#define Y second
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define ip1(a) cin >> a
+#define ip2(a,b) cin >> a >> b
+#define ops cout << ' '
+#define op1(a) cout << a << ' '
+#define op2(a,b) cout << a << ' ' << b
+#define opl cout << '\n'
+#define op1l(a) cout << a << '\n'
+#define op2l(a) cout << a << ' ' << b << '\n'
+
+const int dx[] = { 1,0,-1,0 }, dy[] = { 0,1,0,-1 };
+
+bool OOB(int x, int y, int n, int m) { return x < 0 or x >= n or y < 0 or y >= m; }
+void FastIO() { ios::sync_with_stdio(false); cin.tie(nullptr); }
+
+int ans, n;
+
+int main()
+{
+	FastIO();
+
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			for (int k = 1; k <= n; k++)
+			{
+				if (i + j + k == n)
+				{
+					if (i >= j + 2)
+					{
+						if (i != 0 and j != 0 and k != 0)
+						{
+							if ((k & 1) == 0)
+							{
+								ans++;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	op1l(ans);
+}
